cac1530fe394f3633e81a6fd9371b8a1489ef5b8
Gabriel Bello <gabego@yahoo.com>
2022-11-14 16:23:59 -0500

    Verision 4.5 3D info

--- js/main.js
+++ js/main.js
@@ -23,11 +23,11 @@ header.addEventListener('click', e => {
 
 
 
-const activeDM = document.querySelector('.btn__dm');
+const activeDM = document.querySelector('.btn__dm-sun');
 const fondoDM = document.querySelector('.particles__normal');
 const fondoHD = document.querySelector('.home__data');
 const fondoACT = document.querySelector('.about__container-text');
-const btnDM = document.querySelector('.btn__dm-text');
+const btnDM = document.querySelector('.btn__dm-sun');
 const fondoTC1 = document.querySelector('.tecnologias1__container')
 const fondoTC2 = document.querySelector('.tecnologias2__container')
 const fondoTC3 = document.querySelector('.tecnologias3__container')
@@ -40,7 +40,7 @@ const fondoIB = document.querySelector('.idiomas__box')
 
 activeDM.addEventListener('click', (e) => {
     e.preventDefault();
-    btnDM.classList.toggle('btn__dm-text-dm');
+    btnDM.classList.toggle('btn__dm-moon');
     fondoDM.classList.toggle('particles__dm');
     fondoHD.classList.toggle('dark__mode');
     fondoACT.classList.toggle('dark__mode');
